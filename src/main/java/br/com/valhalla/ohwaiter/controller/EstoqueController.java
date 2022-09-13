eb930c5b4063d80aa518535e55530eb4cd6c437e
Matheus Ribeiro <matheus.junio159@gmail.com>
2022-09-13 20:26:18 -0300

    service de estoque e funcionário

---
+++ src/main/java/br/com/valhalla/ohwaiter/controller/EstoqueController.java
@@ -0,0 +1,5 @@
+package br.com.valhalla.ohwaiter.controller;
+
+public class EstoqueController {
+    
+}
--- src/main/java/br/com/valhalla/ohwaiter/model/Funcionario.java
+++ src/main/java/br/com/valhalla/ohwaiter/model/Funcionario.java
@@ -6,6 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.br.CPF;
 
@@ -17,18 +21,27 @@ public class Funcionario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
+    @NotNull(message = "")
     private String nome;
-    @CPF
+    @NotNull(message = "O CPF deve ser adicionado")
+    @CPF(message = "O CPF deve ser válido")
     private String cpf;
     //Pode se tornar uma tabela no futuro
+    @NotNull(message = "A função é obrigatório")
     private String funcao;
+    @NotNull(message = "O salário é obrigatório")
+    @PositiveOrZero(message = "O salário deve ser de 0 ou maior")
     private BigDecimal salario;
+    @PositiveOrZero(message = "A porcetagem da comissão deve ser maior ou igual a 0")
     private BigDecimal procentagemComissao;
     private Boolean ativo;
     //Pode se tornar uma tabela no futuro
     private String empresaAfiliado;
     //Pode se tornar uma tabela no futuro
+    @NotNull(message = "A permissão é obrigatório")
     private String permissao;
+
+    
     
 }
