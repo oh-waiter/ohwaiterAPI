894f4ad9e0ac532a752d0191293fc5053c425875
Matheus Ribeiro <matheus.junio159@gmail.com>
2022-10-31 17:30:34 -0300

    Começando teste de API

--- src/test/java/br/com/valhalla/ohwaiter/service/FuncionarioServiceTest.java
+++ src/test/java/br/com/valhalla/ohwaiter/service/FuncionarioServiceTest.java
@@ -1,25 +1,46 @@
 package br.com.valhalla.ohwaiter.service;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import br.com.valhalla.ohwaiter.configTest.ApplicationConfigTest;
+import br.com.valhalla.ohwaiter.model.Funcionario;
+import br.com.valhalla.ohwaiter.model.Enums.Funcao;
 import br.com.valhalla.ohwaiter.repository.FuncionarioRepository;
 
-@DisplayName("FuncionarioServiceTest")
-public class FuncionarioServiceTest extends ApplicationConfigTest {
+public class FuncionarioServiceTest {
 
-    @MockBean
+    @Mock
     private FuncionarioRepository funcionarioRepository;
 
-    @Autowired
+    @InjectMocks
     private FuncionarioService funcionarioService;
 
+    private Funcionario funcionario;
+
+    @BeforeEach
+    public void init() {
+        funcionario = Funcionario.builder()
+                .id(1L)
+                .nome("teste")
+                .cpf("98991172008")
+                .ativo(true)
+                .funcao(Funcao.COORDENADOR)
+                .build();
+    }
+
+    @DisplayName("Teste para salvar o funcionario")
     @Test
-    @DisplayName("Remover usuário usando ID")
-    public void deveRemoverUmFuncionarioPeloId() {
+    public void DadoUmFuncionarioSaveOFuncionarioEFacaORetornoDoFuncionario() {
 
     }
+
 }
