a82e5f02ec2815755e706925455d273b80f06ad6
Matheus Ribeiro <matheus.junio159@gmail.com>
2022-11-03 15:35:11 -0300

    Teste Funcioanrio finalizado

--- src/test/java/br/com/valhalla/ohwaiter/service/FuncionarioServiceTest.java
+++ src/test/java/br/com/valhalla/ohwaiter/service/FuncionarioServiceTest.java
@@ -1,16 +1,12 @@
 package br.com.valhalla.ohwaiter.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
-
-import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -18,7 +14,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.valhalla.ohwaiter.model.Funcionario;
@@ -66,11 +61,11 @@ public class FuncionarioServiceTest {
     @DisplayName("Teste para atualizar funcionario")
     public void TesteDeAtualizacaoDeFuncionarios() {
         funcionario = Funcionario.builder()
-        .id(1L)
-        .nome("teste1")
-        .cpf("98991172008")
-        .ativo(true)
-        .funcao(Funcao.COORDENADOR).build();
+                .id(1L)
+                .nome("teste1")
+                .cpf("98991172008")
+                .ativo(true)
+                .funcao(Funcao.COORDENADOR).build();
 
         Funcionario funcionarioUpdate = Funcionario.builder()
                 .id(1L)
@@ -79,59 +74,35 @@ public class FuncionarioServiceTest {
                 .ativo(true)
                 .funcao(Funcao.COORDENADOR)
                 .build();
-        
-       when(funcionarioService.alterarFuncionario(any(Funcionario.class))).thenReturn(funcionarioUpdate);
-       Funcionario funcionarioRetorno = funcionarioService.alterarFuncionario(funcionarioUpdate);
 
-       Assertions.assertThat(funcionarioRetorno).usingRecursiveComparison().isEqualTo(funcionarioUpdate);
-       verify(funcionarioRepository, times(1)).save(any(Funcionario.class));
-       verifyNoMoreInteractions(funcionarioRepository);
-    }
+        when(funcionarioService.alterarFuncionario(any(Funcionario.class))).thenReturn(funcionarioUpdate);
+        Funcionario funcionarioRetorno = funcionarioService.alterarFuncionario(funcionarioUpdate);
 
-    @Test
-    @DisplayName("Buscar todos os funcionarios")
-    public void TesteDeEncontrarTodosOsFuncionarios(){
-        Funcionario funcionario1 = Funcionario.builder()
-        .id(1L)
-        .nome("teste1")
-        .cpf("98991172008")
-        .ativo(true)
-        .funcao(Funcao.COORDENADOR).build();
-
-        Funcionario funcionario2 = Funcionario.builder()
-        .id(2L)
-        .nome("teste1")
-        .cpf("98991172008")
-        .ativo(true)
-        .funcao(Funcao.COORDENADOR).build();
-
-        when(funcionarioService.buscarTodosOsFuncionarios()).thenReturn(List.of(funcionario1, funcionario2));
-
-        List<Funcionario> funcionarios = funcionarioService.buscarTodosOsFuncionarios();
-
-        Assertions.assertThat(funcionarios).isNotNull();
-        Assertions.assertThat(funcionarios.size()).isEqualTo(2);
+        Assertions.assertThat(funcionarioRetorno).usingRecursiveComparison().isEqualTo(funcionarioUpdate);
+        verify(funcionarioRepository, times(1)).save(any(Funcionario.class));
+        verifyNoMoreInteractions(funcionarioRepository);
     }
 
     @Test
     @DisplayName("Excluir Funcionario")
-    public void ExcluirFuncionarioUsandoUmFuncionarioPorParametro(){
+    public void ExcluirFuncionarioUsandoUmFuncionarioPorParametro() {
         Long funcionarioId = 1L;
-        
-        Mockito.doNothing().when(funcionarioService).deletarFuncionarioPorId(anyLong());
+
         funcionarioService.deletarFuncionarioPorId(funcionarioId);
 
-        verify(funcionarioRepository, times(1)).deleteById(anyLong());
+        verify(funcionarioRepository, times(1)).deleteById(funcionarioId);
     }
 
-    @Test
+    @Test()
     @DisplayName("Enviar throw IllegalArgumentException ao enviar um id null")
-    public void ExibirThrowQuandoIdVierNull(){
+    public void ExibirThrowQuandoIdVierNull() {
         Long id = null;
-
-        Mockito.doThrow(IllegalArgumentException.class).when(funcionarioService).deletarFuncionarioPorId(id);
-        funcionarioService.deletarFuncionarioPorId(id);
-
-        verify(funcionarioRepository, never()).deleteById(id);
+        doThrow(new IllegalArgumentException("Id não pode ser nulo")).when(funcionarioRepository)
+                .deleteById(id);
+        try {
+            funcionarioService.deletarFuncionarioPorId(id);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Id não pode ser nulo", e.getMessage());
+        }
     }
 }
