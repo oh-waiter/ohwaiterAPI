f53e310e6c234046a882e675e1852012dc4562b7
Matheus Ribeiro <matheus.junio159@gmail.com>
2022-11-08 17:10:15 -0300

    Início da mensageria

---
+++ src/main/java/br/com/valhalla/ohwaiter/config/QueueConsumer.java
@@ -0,0 +1,15 @@
+package br.com.valhalla.ohwaiter.config;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueueConsumer {
+
+    @RabbitListener(queues = { "${queue.name}" })
+    public void receive(@Payload String fileBody) {
+        System.out.println("message" + fileBody);
+    }
+}
